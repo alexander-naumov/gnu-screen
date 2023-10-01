29d970fd39346a7feab560a4cba74f0501e91f3e
Alexander Naumov <alexander_naumov@opensuse.org>
2023-10-01 19:43:11 +0200

    CI pyte python test: beta

--- tests/pyte_test.py
+++ tests/pyte_test.py
@@ -3,8 +3,9 @@
 import pexpect
 import pyte
 import os
+import re
 
-ROWS, COLS = 45, 160
+ROWS, COLS = 50, 174
 
 screen = pyte.Screen(COLS, ROWS)
 stream = pyte.Stream(screen)
@@ -19,6 +20,7 @@ def spawn_process(cmd):
 
     return pexpect.spawn(cmd, echo=False, encoding='utf-8', dimensions=(ROWS, COLS), env=env)
 
+
 def emulate_ansi_terminal(raw_output, clean=True):
     stream.feed(raw_output)
 
@@ -30,6 +32,7 @@ def emulate_ansi_terminal(raw_output, clean=True):
         lines = (line for line in lines if line)
 
     return '\n'.join(lines)
+
 
 def pprint(out):
     print("-" * COLS)
@@ -43,6 +46,7 @@ def run(T, CMD):
     out = emulate_ansi_terminal(child.before+child.buffer, clean=False)
     pprint(out)
 
+
 def screen_exit():
     child.send('	\\')
     try:
@@ -52,12 +56,26 @@ def screen_exit():
         #print(type(error.args))
         #print(error.args)
 
+
 def screen_detach():
     child.send('	d')
     try:
         run(1, '')
-    except pexpect.exceptions.EOF as error:
-        print(error)
+    except pexpect.exceptions.EOF as err:
+        for i in err.args[0].split('\n'):
+            if re.search("detached", i):
+                ret = i.split(" ")[-1]
+                ret = ret.split("]")[0]
+                return ret
+
+
+def screen_attach(session_id):
+    child = spawn_process('../src/screen -r ' + session_id)
+    child.expect("Password:")
+    child.sendline("pass")
+    out = emulate_ansi_terminal(child.before+child.buffer, clean=False)
+    pprint(out)
+    child.sendline('')
 
 
 child = spawn_process('../src/screen')
@@ -66,26 +84,93 @@ out = emulate_ansi_terminal(child.before+child.buffer, clean=False)
 pprint(out)
 child.sendline('')
 
-child.send('	')
-
+print("\nRun 'pwd':")
 run(1, "pwd")
+
+print ("\nChange PATH to ../src:")
 run(1, "cd ../src/")
+
+print ("\nRun 'ls':")
 run(1, "ls")
+
+print("\nRun 'mc':")
 run(1, "mc")
 
+print("\nSplit screen (ctrl-a |), switch to new window (ctrl-a tab) and create there a new session (ctrl-a c):")
 child.send('|	c')
 
+print("\nRun 'echo $TERM':")
 run(1, "echo $TERM")
-run(5, "ping -c 5 localhost")
+
+print("\nRun 'ping -c 5 localhost':")
+run(1, "ping -c 5 localhost")
 
 #os.system('clear')
 
+print("\nSplit screen (ctrl-a S), switch to new window (ctrl-a tab) and create there a new session (ctrl-a c):")
+child.send('S	c')
+
+print("\nRun 'top':")
 run(1, 'top')
+
+print("come back to first window")
+child.send('	2')
+pprint(emulate_ansi_terminal(child.before+child.buffer, clean=False))
+print("\nsend ctrl-c to 'top':")
 child.send('\x03') #ctrl-c
+run(2, 'true')
+
+print("Swith to 'mc' on first window")
+child.send('0')
 run(1, 'true')
 
-#screen_detach()
+print("\nSplit screen (ctrl-a S), switch to new window (ctrl-a tab) and create there a new session (ctrl-a c):")
+child.send('S	c')
+
+print("\nRun 'ls' on the last window")
+child.sendline('')
+run(1, 'ls -la')
+
+print("\nTest command :next")
+child.send(':next')
+child.sendline('')
+pprint(emulate_ansi_terminal(child.before+child.buffer, clean=False))
+child.send(':next')
+child.sendline('')
+
+
+
+print("Show license:")
+child.send(',')
+pprint(emulate_ansi_terminal(child.before+child.buffer, clean=False))
+child.expect(pexpect.TIMEOUT, timeout=3)
+child.sendline('')
+
+
+print("Test 'list of sessions':")
+child.send('	')
+child.send('"')
+pprint(emulate_ansi_terminal(child.before+child.buffer, clean=False))
+child.expect(pexpect.TIMEOUT, timeout=3)
+pprint(emulate_ansi_terminal(child.before+child.buffer, clean=False))
+child.sendline('')
+child.sendline('')
+#run(1, 'true')
+
+#FIXME
+print("Test 'help':")
+#child.send('	')
+child.send('?')
+pprint(emulate_ansi_terminal(child.before+child.buffer, clean=False))
+child.expect(pexpect.TIMEOUT, timeout=3)
+pprint(emulate_ansi_terminal(child.before+child.buffer, clean=False))
+child.expect(pexpect.TIMEOUT, timeout=3)
+child.sendline('')
+child.sendline('')
+
+run(1, 'true')
+
+#session_id = screen_detach()
+#screen_attach(session_id)
 
 screen_exit()
-
-
