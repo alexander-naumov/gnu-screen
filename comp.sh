c737fbaddf1c4315e08da6765a647fba81f2874c
Alexander Naumov <alexander_naumov@opensuse.org>
2023-10-03 20:54:39 +0200

    some stuff for... private usage

---
+++ comp.sh
@@ -0,0 +1,29 @@
+#/bin/sh
+
+# This script compare two git repositories
+# and shows 'git diff' for all different files
+
+#echo $# (argc)
+#echo $* (argv)
+
+if [ $# != 2 ]; then
+	echo "usage: $0 SAVANNAH_PATH GITHUB_PATH"
+	exit 1
+fi
+
+cd $1
+
+for i in $(ls)
+do
+	if [[ -d $i ]]; then
+		true
+	else
+		echo "$(sha256sum $i | gawk '{ print $1 }') $2$i" | sha256sum --check  >/dev/null
+		if [ $? == 1 ]; then
+			echo
+			echo
+			echo =====================================================================
+			git --no-pager diff --no-index $i $2$i
+		fi
+	fi
+done
