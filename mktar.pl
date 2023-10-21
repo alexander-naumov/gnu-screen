81b3c2c1b368665d836d2a10aa99feba59021a8e
Alexander Naumov <alexander_naumov@opensuse.org>
2023-10-21 19:27:31 +0200

    mktar.pl: get version from the configure.ac file

--- mktar.pl
+++ mktar.pl
@@ -8,7 +8,7 @@
 use Data::Dumper;
 
 my $srcdir = 'src';
-my $version_file = "$srcdir/patchlevel.h";
+my $version_file = "$srcdir/configure.ac";
 my %symlinks = 
 (
   'FAQ' => 'doc/FAQ',
@@ -26,13 +26,9 @@ for my $l (keys %symlinks)
 
 my %version;
 open IN, "<", $version_file or die "unable to read $version_file: $!";
-while (defined(my $line = <IN>))
-  {
-    $version{$1} = (defined $3) ? $3 : $4 if $line =~ m{^#\s*define\s+(\w+)\s+("([^"]*)"|(\S+))};
-  }
+$version = $1 if <IN> =~ /AC_INIT\(\[[^\]]+\],\[(\d+\.\d+\.\d+)\]/;
 close IN;
 
-my $version = "$version{REV}.$version{VERS}.$version{PATCHLEVEL}";
 my $tmpdir = "/tmp/mktar-$^T";
 mkdir $tmpdir or die "cannot mkdir $tmpdir: $!";
 system "cp -a $srcdir $tmpdir" and die "'cp -a $srcdir $tmpdir' failed: $!";
